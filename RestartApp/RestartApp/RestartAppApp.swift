9d374a90d49c071995f04cd3a9c8aaa0531aeeb2
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2023-01-20 04:01:19 +0300

    Added AppIcon and image

---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Sevda Gul Baran on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
